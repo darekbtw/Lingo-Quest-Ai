6f3aead4c33722acac7fa85daf673bc3b0625f4f
Darek Nowak <racingchamp007@gmail.com>
2024-05-04 00:39:26 -0600

    First commit. Basic structure.

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(254), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    join_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+def __repr__(self):
+    return '<task %r>' % self.id
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/learn')
+def learn():
+    return render_template('learn.html')
+
+@app.route('/practice')
+def practice():
+    return render_template('practice.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+if __name__ == "__main__":
+    app.run(debug=True)
